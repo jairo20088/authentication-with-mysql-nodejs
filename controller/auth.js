0e6c925d5758e5bb706a9d081db4cb4f7457321e
Jose Mieses <jairo20088@gmail.com>
2020-05-14 23:29:42 -0500

    added form and some style

--- controller/auth.js
+++ controller/auth.js
@@ -1,10 +1,5 @@
 const User = require("../modal/user");
 
 exports.getAuth = (req, res, next) => {
-  res.send("Hello Jose !!");
-  User.create({
-    name: "Jose Mieses",
-    email: "test@gmail.com",
-    password: "123123123"
-  });
+  res.render("login");
 };
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const authRoute = require("./routes/auth");
 const sequelize = require("./util/database");
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.set("views", "view");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(authRoute);
 
